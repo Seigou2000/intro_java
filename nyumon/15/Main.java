0795a5c37c9d20add25d1cc6170a18169aefc794
seigou2000 <seigou0240@gmail.com>
2025-04-29 21:13:03 +0900

    15, 16章の追加

--- nyumon/15/Main.java
+++ nyumon/15/Main.java
@@ -1,3 +1,10 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        // String s1 = "すっきりjava";
+        String s2 = "Java";
+        String s3 = "java";
+        if (s2.equals(s3)) {
+            System.out.println("s2とs3は等しい");
+        }
+    }
 }
